4ba8593423a068bf69dc848ac5322429607c43dc
Ratte222 <Artur21111999.o@gmail.com>
2021-08-19 17:31:34 +0300

    Добавьте файлы проекта.

---
+++ gRPC Client/Clients/ProductClient.cs
@@ -0,0 +1,81 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gRPC_Client.Clients
+{
+    public class ProductClient
+    {
+        private readonly GrpcChannel _channel;
+        public ProductClient(GrpcChannel channel)
+        {
+            _channel = channel;
+        }
+
+        public async Task<ProductProto> GetProductAsync(long id)
+        {
+            var clientProduct = new Product.ProductClient(_channel);
+            var replyProduct = await clientProduct.GetProductAsync(new ProductRequest { ProductId = id });
+            Console.WriteLine($"Product: {replyProduct.Id}, {replyProduct.Name}, " +
+                $"{replyProduct.Description}, {replyProduct.Cost}");
+            return replyProduct;
+        }
+
+        public async Task<ProductsReply> GetProductsAsync()
+        {
+            var clientProduct = new Product.ProductClient(_channel);
+            var replyProducts = await clientProduct.GetProductsAsync(new ProductsRequest { });
+            Console.WriteLine($"PageResponse: {replyProducts.PageResponse.PageLength}, " +
+                $"{replyProducts.PageResponse.PageNumber}, {replyProducts.PageResponse.ItemCount}, " +
+                $"{replyProducts.PageResponse.TotalItems}, {replyProducts.PageResponse.TotalPages}");
+            Console.WriteLine("Products:");
+            foreach (var res in replyProducts.Products)
+            {
+                Console.WriteLine($"Product: {res.Id}, {res.Name}, " +
+                $"{res.Description}, {res.Cost}");
+            }
+            return replyProducts;
+        }
+
+        public async Task<string> CreateProductAsync()
+        {
+            var client = new Product.ProductClient(_channel);
+            var reply = await client.CreateProductAsync(
+                              new NewProductProto
+                              {
+                                  Name = "Tea",
+                                  Description = "GB Tea",
+                                  Cost = 46
+                              });
+            Console.WriteLine(reply.Message);
+            return reply.Message;
+        }
+
+        public async Task<string> EditProductAsync(long productId)
+        {
+            var client = new Product.ProductClient(_channel);
+            var reply = await client.EditProductAsync(
+                              new ProductProto
+                              {
+                                  Id = productId,
+                                  Name = "Tea",
+                                  Description = "Great Britain Tea",
+                                  Cost = 46
+                              });
+            Console.WriteLine(reply.Message);
+            return reply.Message;
+        }
+
+        public async Task<string> DeleteProductAsync(long productId)
+        {
+            var client = new Product.ProductClient(_channel);
+            var reply = await client.DeleteProductAsync(new ProductRequest { ProductId = productId });
+            Console.WriteLine(reply.Message);
+            return reply.Message;
+        }
+
+    }
+}
---
+++ gRPC Client/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcService;
+using gRPC_Client.Clients;
+using Grpc.Core;
+
+namespace gRPC_Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string _token = await AccountRequests();
+            if (String.IsNullOrEmpty(_token))
+                throw new Exception();
+            var credentials = CallCredentials.FromInterceptor((context, metadata) =>
+            {
+                if (!string.IsNullOrEmpty(_token))
+                {
+                    metadata.Add("Authorization", $"Bearer {_token}");
+                }
+                return Task.CompletedTask;
+            });
+
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions
+            {
+                Credentials = ChannelCredentials.Create(new SslCredentials(), credentials)
+            });
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            
+            await ProductRequests(channel);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static async Task<string> AccountRequests()
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Account.AccountClient(channel);
+            var reply = await client.LoginAsync(
+                              new LoginRequest { EmailOrUserName = "NBA",
+                              Password = "aZ12345678*" });
+            Console.WriteLine($"Login userName: {reply.UserName}");
+            return reply.Token;
+        }
+
+        private static async Task ProductRequests(GrpcChannel channel)
+        {
+            ProductClient productClient = new ProductClient(channel);
+            await productClient.GetProductsAsync();
+            long productId = 3;
+            Console.WriteLine("Testing create product? [y:n]");
+            if (Console.ReadLine() == "y")
+            {
+                await productClient.CreateProductAsync();
+            }
+            Console.WriteLine("Testing update product? [y:n]");
+            if (Console.ReadLine() == "y")
+            {
+                Console.Write("Product before edit: ");
+                await productClient.GetProductAsync(productId);
+                await productClient.EditProductAsync(productId);
+                Console.Write("Product after edit: ");
+                await productClient.GetProductAsync(productId);
+            }
+            Console.WriteLine("Testing delete product? [y:n]");
+            if (Console.ReadLine() == "y")
+            {
+                await productClient.DeleteProductAsync(productId);
+            }
+        }
+    }
+}
